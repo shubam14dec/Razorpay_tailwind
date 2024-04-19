2e20b36131864a7a65922da688aa1e79e83c7946
Shubam Patil <shubamp981@gmail.com>
2024-04-20 00:26:25 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish:["Mulish","sans-serif"],
+      },
+      colors:{
+        deepblue: "#02042a",
+        lightblue:"#2b84ea",
+        lightblue300: "#4b94ed",
+        lightblue500: "#0b72e7",
+        lightgreen: "#61cea6",
+        greytext:"#818597",
+        lightgrey: "#e2e2e2",
+        greyblue:"#344a6c",
+        deepbluehead:"#162f56",
+        grey2:"#525a76",
+      },
+    },
+  },
+  plugins: [],
+}
+
